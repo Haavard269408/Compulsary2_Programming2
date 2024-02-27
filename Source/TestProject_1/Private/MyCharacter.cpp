2e3cd3365442098376baf74bb3da57a10f7398bf
Håvard_Jensen <142876336+Haavard269408@users.noreply.github.com>
2024-02-27 14:18:36 +0100

    Initial commit

---
+++ Source/TestProject_1/Private/MyCharacter.cpp
@@ -0,0 +1,92 @@
+
+#include "MyCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputAction.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	/*Camera Component*/
+	HFJ_CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	HFJ_CameraComponent->SetupAttachment(GetCapsuleComponent());
+	HFJ_CameraComponent->bUsePawnControlRotation = true;
+
+	/*Skeletal Mesh Component*/
+	HFJ_Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharMesh"));
+	HFJ_Mesh->SetupAttachment(HFJ_CameraComponent);
+}
+
+void AMyCharacter::Move(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Emerald, TEXT("Triggering the move function"));
+
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+
+void AMyCharacter::LookAround(const FInputActionValue& Value)
+{
+
+	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Emerald, TEXT("Looking"));
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+
+
+}
+
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Adding the Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(IMC, 0);
+		}
+	}
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMyCharacter::LookAround);
+
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		
+	}
+
+}
+
